5eb9a963ee3e290adfe538d222ad84bdcc59a841
imrajputkartik <kartikchauhan647@gmail.com>
2025-11-18 13:01:02 +0530

    update

--- src/main/java/com/lavish/spring/model/Employee.java
+++ src/main/java/com/lavish/spring/model/Employee.java
@@ -1,42 +1,59 @@
 package com.lavish.spring.model;
 
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 public class Employee {
 //Instance Var / Attribute / Properties
 	private String name, gender;
 	private int id;
 	private Address address;
-	
+
+	private List<Integer> list;
+	private Set<Integer> set;
+	private Map<Integer, String> map;
+
 	public Employee() {
 		super();
 		// TODO Auto-generated constructor stub
 		System.out.println("Employee.employee()");
 	}
-	
-	public Employee(String name, String gender, int id, Address address) {
+
+	public Employee(String name, String gender, int id, Address address, List<Integer> list, Set<Integer> set,
+			Map<Integer, String> map) {
 		super();
 		this.name = name;
 		this.gender = gender;
 		this.id = id;
 		this.address = address;
+		this.list = list;
+		this.set = set;
+		this.map = map;
 	}
 
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 		System.out.println("Employee.setName()");
 	}
+
 	public String getGender() {
 		return gender;
 	}
+
 	public void setGender(String gender) {
 		this.gender = gender;
 		System.out.println("Employee.setGender()");
 	}
+
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 		System.out.println("Employee.setId()");
@@ -50,11 +67,34 @@ public class Employee {
 		this.address = address;
 	}
 
+	public List<Integer> getList() {
+		return list;
+	}
+
+	public void setList(List<Integer> list) {
+		this.list = list;
+	}
+
+	public Set<Integer> getSet() {
+		return set;
+	}
+
+	public void setSet(Set<Integer> set) {
+		this.set = set;
+	}
+
+	public Map<Integer, String> getMap() {
+		return map;
+	}
+
+	public void setMap(Map<Integer, String> map) {
+		this.map = map;
+	}
+
 	@Override
 	public String toString() {
-		return "Employee [name=" + name + ", gender=" + gender + ", id=" + id + ", address=" + address + "]";
+		return "Employee [name=" + name + ", gender=" + gender + ", id=" + id + ", address=" + address + ", list="
+				+ list + ", set=" + set + ", map=" + map + "]";
 	}
-	
-	
-	
+
 }
