9c849083e66ee4af85ef3257c45b24424e0ae70e
imrajputkartik <kartikchauhan647@gmail.com>
2025-11-18 15:08:24 +0530

    update

--- src/main/java/com/lavish/spring/Main.java
+++ src/main/java/com/lavish/spring/Main.java
@@ -14,9 +14,6 @@ public class Main {
 		
 		Employee bean= ioc.getBean("e1", Employee.class);
 		System.out.println(bean);
-		
-		Employee bean2= ioc.getBean("e2", Employee.class);
-		System.out.println(bean2);
 
 	}
 
--- src/main/java/com/lavish/spring/model/Employee.java
+++ src/main/java/com/lavish/spring/model/Employee.java
@@ -1,46 +1,50 @@
 package com.lavish.spring.model;
 
 public class Employee {
-//Instance Var / Attribute / Properties
+
+	//Instance Var / Attribute / Properties
+	
 	private String name, gender;
 	private int id;
+	private Address address;
 	public Employee() {
 		super();
 		// TODO Auto-generated constructor stub
-		System.out.println("Employee.employee()");
 	}
-	public Employee(String name, String gender, int id) {
+	public Employee(String name, String gender, int id, Address address) {
 		super();
 		this.name = name;
 		this.gender = gender;
 		this.id = id;
-		
-		System.out.println("Employee.employee(3)");
+		this.address = address;
 	}
 	public String getName() {
 		return name;
 	}
 	public void setName(String name) {
 		this.name = name;
-		System.out.println("Employee.setName()");
 	}
 	public String getGender() {
 		return gender;
 	}
 	public void setGender(String gender) {
 		this.gender = gender;
-		System.out.println("Employee.setGender()");
 	}
 	public int getId() {
 		return id;
 	}
 	public void setId(int id) {
 		this.id = id;
-		System.out.println("Employee.setId()");
+	}
+	public Address getAddress() {
+		return address;
+	}
+	public void setAddress(Address address) {
+		this.address = address;
 	}
 	@Override
 	public String toString() {
-		return "Employee [name=" + name + ", gender=" + gender + ", id=" + id + "]";
+		return "Employee [name=" + name + ", gender=" + gender + ", id=" + id + ", address=" + address + "]";
 	}
 	
 	
